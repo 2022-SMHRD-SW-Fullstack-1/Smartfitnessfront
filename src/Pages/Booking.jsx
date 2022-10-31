7cdbcec1485381d83e1a642028747b43f1f67770
Nuri-Kim <allmonde1994@gmail.com>
2022-10-31 15:17:23 +0900

    캘린더삭제

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import App from '../5vdvtr/src/App'
 
 const Booking = () => {
   return (
-    <div>Booking</div>
+    <div className='bookingPage'>
+      부킹페이지
+      <App/>
+    </div>
   )
 }
 
